fd92d6f0b9edc2b709d499fe3b6c26cd99736453
Keteli Lino <ketelilino@gmail.com>
2025-11-10 13:35:22 -0300

    .

--- Thanatos_app/frm_login.Designer.vb
+++ Thanatos_app/frm_login.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frm_login
-    Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()>
@@ -22,17 +22,151 @@ Partial Class frm_login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_login))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GunaShadowPanel1 = New Guna.UI.WinForms.GunaShadowPanel()
+        Me.BunifuCustomLabel2 = New ns1.BunifuCustomLabel()
+        Me.BunifuCustomLabel1 = New ns1.BunifuCustomLabel()
+        Me.txt_senha = New Guna.UI.WinForms.GunaTextBox()
+        Me.txt_usuario = New Guna.UI.WinForms.GunaTextBox()
+        Me.btn_entrar = New Guna.UI.WinForms.GunaButton()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GunaShadowPanel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 21)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(46, 43)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'GunaShadowPanel1
+        '
+        Me.GunaShadowPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaShadowPanel1.BaseColor = System.Drawing.Color.Gray
+        Me.GunaShadowPanel1.Controls.Add(Me.btn_entrar)
+        Me.GunaShadowPanel1.Controls.Add(Me.BunifuCustomLabel2)
+        Me.GunaShadowPanel1.Controls.Add(Me.BunifuCustomLabel1)
+        Me.GunaShadowPanel1.Controls.Add(Me.txt_senha)
+        Me.GunaShadowPanel1.Controls.Add(Me.txt_usuario)
+        Me.GunaShadowPanel1.Location = New System.Drawing.Point(283, 173)
+        Me.GunaShadowPanel1.Name = "GunaShadowPanel1"
+        Me.GunaShadowPanel1.Radius = 6
+        Me.GunaShadowPanel1.ShadowColor = System.Drawing.Color.Black
+        Me.GunaShadowPanel1.Size = New System.Drawing.Size(619, 363)
+        Me.GunaShadowPanel1.TabIndex = 1
+        '
+        'BunifuCustomLabel2
+        '
+        Me.BunifuCustomLabel2.AutoSize = True
+        Me.BunifuCustomLabel2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BunifuCustomLabel2.ForeColor = System.Drawing.Color.White
+        Me.BunifuCustomLabel2.Location = New System.Drawing.Point(106, 171)
+        Me.BunifuCustomLabel2.Name = "BunifuCustomLabel2"
+        Me.BunifuCustomLabel2.Size = New System.Drawing.Size(74, 25)
+        Me.BunifuCustomLabel2.TabIndex = 3
+        Me.BunifuCustomLabel2.Text = "Senha"
+        '
+        'BunifuCustomLabel1
+        '
+        Me.BunifuCustomLabel1.AutoSize = True
+        Me.BunifuCustomLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BunifuCustomLabel1.ForeColor = System.Drawing.Color.White
+        Me.BunifuCustomLabel1.Location = New System.Drawing.Point(106, 47)
+        Me.BunifuCustomLabel1.Name = "BunifuCustomLabel1"
+        Me.BunifuCustomLabel1.Size = New System.Drawing.Size(86, 25)
+        Me.BunifuCustomLabel1.TabIndex = 2
+        Me.BunifuCustomLabel1.Text = "Usuário"
+        '
+        'txt_senha
+        '
+        Me.txt_senha.BackColor = System.Drawing.Color.Transparent
+        Me.txt_senha.BaseColor = System.Drawing.Color.White
+        Me.txt_senha.BorderColor = System.Drawing.Color.White
+        Me.txt_senha.BorderSize = 0
+        Me.txt_senha.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txt_senha.FocusedBaseColor = System.Drawing.Color.White
+        Me.txt_senha.FocusedBorderColor = System.Drawing.Color.Gray
+        Me.txt_senha.FocusedForeColor = System.Drawing.SystemColors.ControlText
+        Me.txt_senha.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.txt_senha.Location = New System.Drawing.Point(109, 199)
+        Me.txt_senha.Name = "txt_senha"
+        Me.txt_senha.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txt_senha.SelectedText = ""
+        Me.txt_senha.Size = New System.Drawing.Size(385, 53)
+        Me.txt_senha.TabIndex = 1
+        '
+        'txt_usuario
+        '
+        Me.txt_usuario.BackColor = System.Drawing.Color.Gray
+        Me.txt_usuario.BaseColor = System.Drawing.Color.White
+        Me.txt_usuario.BorderColor = System.Drawing.Color.White
+        Me.txt_usuario.BorderSize = 0
+        Me.txt_usuario.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txt_usuario.FocusedBaseColor = System.Drawing.Color.White
+        Me.txt_usuario.FocusedBorderColor = System.Drawing.Color.Gray
+        Me.txt_usuario.FocusedForeColor = System.Drawing.SystemColors.ControlText
+        Me.txt_usuario.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.txt_usuario.Location = New System.Drawing.Point(109, 74)
+        Me.txt_usuario.Name = "txt_usuario"
+        Me.txt_usuario.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txt_usuario.SelectedText = ""
+        Me.txt_usuario.Size = New System.Drawing.Size(385, 53)
+        Me.txt_usuario.TabIndex = 0
+        '
+        'btn_entrar
+        '
+        Me.btn_entrar.AnimationHoverSpeed = 0.07!
+        Me.btn_entrar.AnimationSpeed = 0.03!
+        Me.btn_entrar.BaseColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(117, Byte), Integer), CType(CType(202, Byte), Integer))
+        Me.btn_entrar.BorderColor = System.Drawing.Color.Black
+        Me.btn_entrar.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.btn_entrar.FocusedColor = System.Drawing.Color.Empty
+        Me.btn_entrar.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.btn_entrar.ForeColor = System.Drawing.Color.White
+        Me.btn_entrar.Image = Nothing
+        Me.btn_entrar.ImageSize = New System.Drawing.Size(20, 20)
+        Me.btn_entrar.Location = New System.Drawing.Point(222, 277)
+        Me.btn_entrar.Name = "btn_entrar"
+        Me.btn_entrar.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn_entrar.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.btn_entrar.OnHoverForeColor = System.Drawing.Color.White
+        Me.btn_entrar.OnHoverImage = Nothing
+        Me.btn_entrar.OnPressedColor = System.Drawing.Color.Black
+        Me.btn_entrar.Radius = 6
+        Me.btn_entrar.Size = New System.Drawing.Size(160, 42)
+        Me.btn_entrar.TabIndex = 4
+        Me.btn_entrar.Text = "Entrar"
+        Me.btn_entrar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'frm_login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1197, 755)
+        Me.ClientSize = New System.Drawing.Size(1180, 679)
+        Me.Controls.Add(Me.GunaShadowPanel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frm_login"
-        Me.Text = "Login"
+        Me.Text = "           Thanatos"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GunaShadowPanel1.ResumeLayout(False)
+        Me.GunaShadowPanel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GunaShadowPanel1 As Guna.UI.WinForms.GunaShadowPanel
+    Friend WithEvents txt_usuario As Guna.UI.WinForms.GunaTextBox
+    Friend WithEvents txt_senha As Guna.UI.WinForms.GunaTextBox
+    Friend WithEvents BunifuCustomLabel1 As ns1.BunifuCustomLabel
+    Friend WithEvents BunifuCustomLabel2 As ns1.BunifuCustomLabel
+    Friend WithEvents btn_entrar As Guna.UI.WinForms.GunaButton
 End Class
--- Thanatos_app/frm_login.vb
+++ Thanatos_app/frm_login.vb
@@ -1,5 +1,50 @@
 ﻿Imports MaterialSkin
 Imports MaterialSkin.Controls
+
 Public Class frm_login
-    Inherits MaterialSkin.Controls.MaterialForm
+    ' Se você preferir instanciar uma vez:
+    Private ReadOnly materialSkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+
+    Public Sub New()
+        InitializeComponent()
+
+        ' Registra o form no MaterialSkinManager
+        materialSkinManager.AddFormToManage(Me)
+
+        ' Tema: LIGHT ou DARK
+        materialSkinManager.Theme = MaterialSkinManager.Themes.DARK
+
+        ' Exemplo: usando hex diretamente via ColorTranslator.FromHtml
+        materialSkinManager.ColorScheme = New ColorScheme(
+            ColorTranslator.FromHtml("#802790"), ' Primary
+            ColorTranslator.FromHtml("#802790"), ' Dark Primary
+            ColorTranslator.FromHtml("#802790"), ' Light Primary
+            ColorTranslator.FromHtml("#802790"), ' Accent
+            TextShade.WHITE)
+    End Sub
+
+    Private Sub frm_login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim pb As PictureBox = FindFirstPictureBox(Me)
+        If pb IsNot Nothing Then
+            pb.BackColor = ColorTranslator.FromHtml("#802790") ' ajuste o HEX aqui
+            pb.SizeMode = PictureBoxSizeMode.StretchImage
+            pb.BorderStyle = BorderStyle.None
+        End If
+    End Sub
+
+    Private Function FindFirstPictureBox(parent As Control) As PictureBox
+        For Each c As Control In parent.Controls
+            If TypeOf c Is PictureBox Then
+                Return DirectCast(c, PictureBox)
+            End If
+            If c.HasChildren Then
+                Dim f = FindFirstPictureBox(c)
+                If f IsNot Nothing Then Return f
+            End If
+        Next
+        Return Nothing
+    End Function
+
+
+    ' Resto do código do form...
 End Class
